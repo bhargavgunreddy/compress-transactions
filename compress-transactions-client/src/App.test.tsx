1ff51ccb98c3ebd17e07ba53cd4901153e8ce8d6
bhargavgunreddy <gbhargavreddy@gmail.com>
2023-05-26 13:22:44 -0400

    Fix test cases to remove unused dependencies

--- compress-transactions-client/src/App.test.tsx
+++ compress-transactions-client/src/App.test.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { setupServer } from 'msw/node';
-import { rest } from 'msw'
 
-const server = setupServer(
-  rest.get('/login', (req, res, ctx) => {
-    return res(ctx.json({ success: 'success' }))
-  }),
-)
+
 
 test('renders The login component', () => {
   render(<App />);
